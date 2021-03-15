4909b5dedca035429a1d14871a5845ebc42ce9a0
Lukas Zbinden <lukas.zbinden@unifr.ch>
2021-03-15 09:12:52 +0100

    initial commit

---
+++ rsi.py
@@ -0,0 +1,39 @@
+import pandas as pd
+import numpy as np
+
+
+def rsi_tradingview(ohlc: pd.DataFrame, period: int = 14):
+    """ Implements the RSI indicator as defined by TradingView on March 15, 2021.
+        The TradingView code is as follows:
+        //@version=4
+        study(title="Relative Strength Index", shorttitle="RSI", format=format.price, precision=2, resolution="")
+        len = input(14, minval=1, title="Length")
+        src = input(close, "Source", type = input.source)
+        up = rma(max(change(src), 0), len)
+        down = rma(-min(change(src), 0), len)
+        rsi = down == 0 ? 100 : up == 0 ? 0 : 100 - (100 / (1 + up / down))
+        plot(rsi, "RSI", color=#8E1599)
+        band1 = hline(70, "Upper Band", color=#C0C0C0)
+        band0 = hline(30, "Lower Band", color=#C0C0C0)
+        fill(band1, band0, color=#9915FF, transp=90, title="Background")
+
+    :param ohlc:
+    :param period:
+    :return: an array with the RSI indicator values
+    """
+
+    delta = ohlc["close"].diff()
+
+    up = delta.copy()
+    up[up < 0] = 0
+    up = pd.Series.ewm(up, alpha=1/period).mean()
+
+    down = delta.copy()
+    down[down > 0] = 0
+    down *= -1
+    down = pd.Series.ewm(down, alpha=1/period).mean()
+
+    rsi = np.where(up == 0, 0, np.where(down == 0, 100, 100 - (100 / (1 + up / down))))
+
+    return np.round(rsi, 2)
+
